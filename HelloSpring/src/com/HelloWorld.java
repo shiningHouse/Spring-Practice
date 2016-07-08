325701676343d1efc2e8a090275eed1cd36fee33
shiningHouse <huangdy7@mail2.sysu.edu.cn>
2016-07-08 14:06:08 +0800

    Hello Spring

---
+++ HelloSpring/src/com/HelloWorld.java
@@ -0,0 +1,15 @@
+package com;
+
+public class HelloWorld {
+	private String message;
+	
+	public void setMessage(String message)
+	{
+		this.message = message;
+	}
+	
+	public String getMessgae()
+	{
+		return message;
+	}
+}
